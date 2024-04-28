aaebad53c787c846beffdf337fc268bfdc9efa39
gracehuynhh <gracehuynh98@gmail.com>
2024-04-28 17:29:17 +0700

    new

---
+++ classic/connect/config.php
@@ -0,0 +1,28 @@
+<?php
+
+$web_config = array(
+	'url' => 'https://chromedino.io',
+	'title' => 'Play Doodle Jump',
+	'name' => 'Doodle Jump',
+	'desc' => 'Play Doodle Jump for free online in HTML5 (No ADS)',
+	'thumb' => 'https://doodlejump.io/images/doodle-jump-game-card.png',
+	'prefix' => 'doodle-jump'
+	
+);
+
+
+
+$config = array(
+	'dbHost' => '156.67.64.10',
+	'dbUser' => 'u394440496_webgame',
+	'dbPass' => '^2j!@f@U',
+	'dbName' => 'u394440496_webgame',
+	'dbOptions' => array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+    )
+);
+
+
+
+
+?>
